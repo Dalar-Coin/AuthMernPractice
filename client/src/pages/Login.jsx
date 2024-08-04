29521528b938f0adcf92d2db00dddef7c9f9ff09
Dalar-Coin <77028982+Dalar-Coin@users.noreply.github.com>
2024-08-03 21:17:30 -0400

    small update

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -24,7 +24,9 @@ export default function Login() {
         setData({});
         navigate("/");
       }
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
--- server/Index.js
+++ server/Index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const { mongoose } = require("mongoose");
+const cookieParser = require("cookie-parser");
 const app = express();
-// database connect
+// database connection
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Database Connected"))
@@ -11,6 +12,10 @@ mongoose
 
 //middleware
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", require("./Routes/authRoutes"));
 
 app.use("/", require("./Routes/authRoutes"));
 
